65c202da741b1fbcb7d40e8e103ec8412913865e
blessingmichaels <blessingmichaels17@gmail.com>
2024-06-12 05:59:22 -0700

    minor changes

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -48,7 +48,7 @@ const Todo = () => {
     },[todoList])
 
     return (
-        <div className='bg-white place-self-center w-12/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl'>
+        <div className='bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl'>
 
 
             {/* ---------title------- */}
@@ -62,7 +62,7 @@ const Todo = () => {
 
             {/* ---------input box------- */}
 
-            <div className='flex items-center my-7 bg-gray-200 rounded-full'>
+            <div className='flex items-center my-7 bg-gray-200 rounded-full w-auto'>
                 <input ref={inputRef} className='bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2
                  placeholder:text-slate-600 '
                     type="text" placeholder='Add your task' />
